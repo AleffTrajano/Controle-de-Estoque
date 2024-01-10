ffef036bbfa0b28df6dadf111769e2ad77a09198
aleff <aleff_diniz@hotmail.com>
2024-01-10 15:45:10 -0300

    Criando o formulario reativo

--- src/app/modules/home/home.component.ts
+++ src/app/modules/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-home',
@@ -8,4 +9,31 @@ import { Component } from '@angular/core';
 export class HomeComponent {
 
   loginCard = true;
+
+  loginForm = this.formBuilder.group({
+    email:['',Validators.required],
+    password:['',Validators.required],
+
+  })
+
+  signupForm =this.formBuilder.group({
+    name:['',Validators.required],
+    email:['',Validators.required],
+    password:['',Validators.required],
+  })
+
+  constructor(private formBuilder:FormBuilder){}
+
+  onSubmitLoginForm(): void {
+    console.log(
+      'DADOS DO FORMULARIO DE LOGIN',
+       this.loginForm.value);
+  }
+
+  onSubmitSignupForm(): void {
+    console.log(
+      'DADOS DA CRIAÇÃO DE CONTAS',
+       this.signupForm.value);
+  }
+
 }
